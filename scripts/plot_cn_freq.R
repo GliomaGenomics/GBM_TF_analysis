fa9eb44bcb395f039fdf519632e9d9dee5464e50
GeorgetteTanner <medgnt@leeds.ac.uk>
2022-02-03 18:56:17 +0000

    remove_copy_number

--- scripts/plot_cn_freq.R
+++ scripts/plot_cn_freq.R
@@ -35,14 +35,16 @@ write.table(down_prim,file="copy_number/down_prim_cn.txt",row.names = FALSE,quot
 write.table(down_recu,file="copy_number/down_recu_cn.txt",row.names = FALSE,quote = FALSE, sep='\t')
 
 
-thresh=0.05
+thresh=0.1
 pdf(paste("copy_number/plotFreq",thresh,".pdf",sep=""))
-plotFreq(up_prim,thres.gain=thresh,thres.loss=-thresh,xlab='up_prim',chrom=16)
-plotFreq(down_prim,thres.gain=thresh,thres.loss=-thresh,xlab='down_prim',chrom=16)
-plotFreq(up_recu,thres.gain=thresh,thres.loss=-thresh,xlab='up_recu',chrom=16)
-plotFreq(down_recu,thres.gain=thresh,thres.loss=-thresh,xlab='down_recu',chrom=16)
-plotAberration(up_prim,thres.gain=thresh,thres.loss=-thresh,xlab='up_prim',chrom=16)
-plotAberration(down_prim,thres.gain=thresh,thres.loss=-thresh,xlab='down_prim',chrom=16)
-plotAberration(up_recu,thres.gain=thresh,thres.loss=-thresh,xlab='up_recu',chrom=16)
-plotAberration(down_recu,thres.gain=thresh,thres.loss=-thresh,xlab='down_recu',chrom=16)
+plotFreq(up_prim,thres.gain=thresh,thres.loss=-thresh,xlab='up_prim',chrom=6)
+plotFreq(down_prim,thres.gain=thresh,thres.loss=-thresh,xlab='down_prim',chrom=6)
+plotFreq(up_recu,thres.gain=thresh,thres.loss=-thresh,xlab='up_recu',chrom=6)
+plotFreq(down_recu,thres.gain=thresh,thres.loss=-thresh,xlab='down_recu',chrom=6)
+
+chro=6
+plotAberration(up_prim,thres.gain=thresh,thres.loss=-thresh,xlab='up_prim',chrom=chro)
+plotAberration(down_prim,thres.gain=thresh,thres.loss=-thresh,xlab='down_prim',chrom=chro)
+plotAberration(up_recu,thres.gain=thresh,thres.loss=-thresh,xlab='up_recu',chrom=chro)
+plotAberration(down_recu,thres.gain=thresh,thres.loss=-thresh,xlab='down_recu',chrom=chro)
 dev.off()
